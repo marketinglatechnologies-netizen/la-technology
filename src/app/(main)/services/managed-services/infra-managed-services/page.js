08a6f013b8fb76d3a1e4355026263c6d8eafc86e
SyspreeDev <development@syspreedigital.com>
2026-01-23 11:28:49 +0530

    image replaced

--- src/app/(main)/services/managed-services/infra-managed-services/page.js
+++ src/app/(main)/services/managed-services/infra-managed-services/page.js
@@ -114,7 +114,7 @@ infrastructure so you can focus on innovation and growth."
 
       <WhyChooseSection
         heading="What LA Technologies Provide as Solutions:"
-        centerImage="/assets/images/services/infra-managed-services/whychoose.jpg"
+        centerImage="/assets/images/services/infra-managed-services/center.jpg"
         featuresLeft={[
           {
             icon: "/assets/images/services/infra/choose1.png",
