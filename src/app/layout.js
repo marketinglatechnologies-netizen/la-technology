a1d1b5843cea6d898c256f3fca702091ca7de57e
SyspreeDev <development@syspreedigital.com>
2026-01-29 17:43:34 +0530

    gtm added

--- src/app/layout.js
+++ src/app/layout.js
@@ -43,10 +43,10 @@ export default function RootLayout({ children }) {
       <Script id="google-tag-manager" strategy="afterInteractive">
         {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5RCTSBHK');
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5RCTSBHK');
           `}
       </Script>
       <body
@@ -57,8 +57,8 @@ export default function RootLayout({ children }) {
             src="https://www.googletagmanager.com/ns.html?id=GTM-5RCTSBHK"
             height="0"
             width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
+            style="display:none;visibility:hidden"
+          ></iframe>
         </noscript>
 
         <Suspense fallback={null}>
