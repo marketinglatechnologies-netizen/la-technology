764849b3d1b9b8bddf2b3fd3f0875517190b3c84
SyspreeDev <development@syspreedigital.com>
2026-06-29 16:50:31 +0530

    Fix Next.js build

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
-    output: "export",
-      images: {
+  images: {
     unoptimized: true,
   },
 };
-
-// next.config.js
-// module.exports = {
-//   output: 'export',
-// };
 
 export default nextConfig;
